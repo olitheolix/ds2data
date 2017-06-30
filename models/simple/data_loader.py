e8ae280cb223beff027c147be9c1aa0c52fcdc67
Oliver Nagy <olitheolix@gmail.com>
2017-06-30 18:21:00 +1000

    Added RPN loader.

--- models/simple/data_loader.py
+++ models/simple/data_loader.py
@@ -1,5 +1,4 @@
-""" A uniform interface to request images.
-"""
+""" A uniform interface to request images."""
 import os
 import glob
 import collections
@@ -69,11 +68,10 @@ class DataSet:
         assert isinstance(x, np.ndarray)
         assert isinstance(y, np.ndarray)
         assert x.dtype == np.uint8, x.dtype
-        assert y.dtype == np.int32, y.dtype
 
         # Sanity check: images must be a 4-D tensor, and there must be as many
         # labels as there are features (images).
-        assert x.ndim == 4 and y.ndim == 1
+        assert x.ndim == 4
         assert x.shape[0] == y.shape[0]
 
         # Sanity check: to comply with the NCHW format, the second to fourth
@@ -361,3 +359,222 @@ class DS2(DataSet):
         all_labels = np.array(all_labels, np.int32)
 
         return all_features, all_labels, dims, label2name, meta
+
+
+def loadObjects(N=32, chan=3):
+    out = np.zeros((2, chan, N, N), np.uint8)
+
+    # First shape is a box.
+    out[0, :, 1:-1, 1:-1] = 255
+
+    # Second shape is a disc.
+    centre = N / 2
+    for y in range(N):
+        for x in range(N):
+            dist = np.sqrt(((x - centre) ** 2 + (y - centre) ** 2))
+            out[1, :, y, x] = 255 if dist < (N - 2) / 2 else 0
+    return out
+
+
+class FasterRcnnRpn(DataSet):
+    """ Create training images with randomly placed objects.
+
+    This class will not only produce the training images but also the
+    target values for the RPN. Specifically, it will provide the overlap of
+    each BBox with the anchor and the precise dimensions of the BBox.
+    """
+    def loadRawData(self):
+        # Original attributes of the images in the DS2 dataset.
+        N = self.conf.num_samples
+        col_fmt = 'RGB'
+
+        width = self.conf.width or 128
+        height = self.conf.height or 128
+        col_fmt = self.conf.colour or 'RGB'
+        col_fmt = col_fmt.upper()
+        assert col_fmt in {'RGB', 'L'}
+        chan = 1 if col_fmt == 'L' else 3
+
+        # The size of the returned images.
+        dims = (chan, height, width)
+
+        label2name = None
+
+        # Location to data folder.
+        data_path = os.path.dirname(os.path.abspath(__file__))
+        data_path = os.path.join(data_path, 'data', 'training')
+        data_path = '/tmp/delme/flightpath'
+
+        # Iterate over all labels. Each label must reside in its own directory.
+        all_labels, all_features, meta = [], [], []
+
+        fnames = []
+        for ext in ['jpg', 'JPG', 'jpeg', 'JPEG']:
+            fnames.extend(glob.glob(f'{data_path}/*.' + ext))
+        del ext
+
+        # Abort if the data set does not exist.
+        if len(fnames) == 0:
+            # fixme: correct data path
+            print(f'\nError: No files in {data_path}')
+            print('\nPlease download '
+                  'https://github.com/olitheolix/ds2data/blob/master/ds2.tar.gz'
+                  '\nand unpack it to data/\n')
+            raise FileNotFoundError
+
+        # Load each image, pre-process it (eg resize, RGB/Gray), and add it
+        # to the data set.
+        for i, fname in enumerate(fnames[:N]):
+            # Convert to correct colour format and resize.
+            img = Image.open(fname)
+            img = img.convert(col_fmt)
+            if img.size != (width, height):
+                img = img.resize((width, height), Image.BILINEAR)
+
+            # We work in NumPy from now on.
+            img = np.array(img, np.uint8)
+
+            # Insert a dummy dimension for grayscale (2d images).
+            if img.ndim == 2:
+                img = np.expand_dims(img, axis=2)
+
+            # Move the colour dimension to the front, ie convert a
+            # (height x width x chan) image to (chan x height x width).
+            assert img.shape == (height, width, chan)
+            img = np.transpose(img, [2, 0, 1])
+            assert img.shape == dims == (chan, height, width)
+
+            # Place objects.
+            img, bboxes = self.placeObjects(img, num_placements=2)
+            assert img.shape == dims
+            assert bboxes.dtype == np.uint32
+            assert bboxes.shape[1] == 4
+
+            # Compile a list of RPN training data based on the bboxes of the
+            # objects in the image.
+            label_mr = self.bbox2RPNLabels(bboxes, (height, width))
+
+            # Store the flattened image alongside its label and meta data.
+            all_labels.append(label_mr)
+            all_features.append(img)
+            meta.append(MetaData(fname, label_mr, None))
+
+            # fixme
+            print('debug: Only loading one image')
+            break
+
+        # Ensure that everything is a proper NumPy array.
+        all_features = np.array(all_features, np.uint8)
+        all_labels = np.array(all_labels, np.float32)
+
+        return all_features, all_labels, dims, label2name, meta
+
+    def placeObjects(self, img, num_placements):
+        assert img.dtype == np.uint8
+        assert img.ndim == 3
+        assert num_placements >= 0
+
+        chan, width, height = img.shape
+        assert chan in [1, 3]
+
+        objs = loadObjects(N=32, chan=chan)
+        pool_size, obj_chan, obj_height, obj_width = objs.shape
+        assert obj_chan == chan
+
+        box_img = np.zeros((height, width), np.uint8)
+
+        bbox = []
+        miss = 0
+        while len(bbox) < num_placements:
+            # Pick random coordinate for object (upper left corner).
+            x0 = np.random.randint(0, width - obj_width)
+            y0 = np.random.randint(0, height - obj_height)
+            x1, y1 = x0 + obj_width, y0 + obj_height
+            if np.max(box_img[y0:y1, x0:x1]) != 0:
+                miss += 1
+                if miss > 100:
+                    print(f'Warning: could not place all {num_placements}')
+                    break
+                continue
+
+            box_img[y0:y1, x0:x1] = 1
+
+            obj = objs[np.random.randint(0, pool_size)]
+            img[:, y0:y1, x0:x1] = obj
+            bbox.append((x0, x1, y0, y1))
+
+        bbox = np.array(bbox, np.uint32)
+        return img, bbox
+
+    def bbox2RPNLabels(self, bboxes, dims_hw, downsample=4):
+        assert bboxes.shape[1] == 4
+        assert isinstance(downsample, int) and downsample >= 1
+
+        im_height, im_width = dims_hw
+        ft_height, ft_width = im_height // downsample, im_width // downsample
+        out = np.zeros((7, ft_height, ft_width), np.float32)
+
+        anchor_hwidth, anchor_hheight = 16, 16
+        a_width, a_height = 2 * anchor_hwidth + 1, 2 * anchor_hheight + 1
+
+        for y in range(ft_height):
+            for x in range(ft_width):
+                # Compute anchor box coordinates in original image.
+                acx, acy = x * downsample, y * downsample
+                acx, acy = acx + downsample // 2, acy + downsample // 2
+                ax0, ay0 = acx - anchor_hwidth, acy - anchor_hheight
+                ax1, ay1 = acx + anchor_hwidth, acy + anchor_hheight
+
+                # Ignore the current position if the anchor box is not fully
+                # inside the image.
+                if ax0 < 0 or ax1 >= im_width or ay0 < 0 or ay1 >= im_height:
+                    out[0:3, y, x] = [0, 1, 0]
+                    continue
+
+                # Mark this region as a valid one (ie the anchor box is not
+                # clipped in any direction).
+                out[0, y, x] = 1
+
+                # Find out which BBox (if any) has the highest IoU:
+                max_iou, bbox = 0.7, None
+                sa = {(x, y) for x in range(ax0, ax1 + 1) for y in range(ay0, ay1 + 1)}
+                for x0, x1, y0, y1 in bboxes:
+                    sb = {(x, y) for x in range(x0, x1 + 1) for y in range(y0, y1 + 1)}
+                    inter = len(sa.intersection(sb))
+                    union = len(sa.union(sb))
+
+                    # Do nothing if there is no overlap.
+                    if union == 0:
+                        continue
+
+                    # Compute the IoU ratio. If it exceeds 0.7 add the current
+                    # bbox to the candidate list.
+                    iou = inter / union
+                    if iou > max_iou:
+                        max_iou = iou
+                        bbox = x0, x1, y0, y1
+
+                # Mark the area as "no-object" if the anchor did not
+                # sufficiently overlap with an object. Then move on to the next
+                # object.
+                if bbox is None:
+                    out[1:3, y, x] = [1, 0]
+                    continue
+
+                # If we get to here it means the anchor has sufficient overlap
+                # with at least one object. Therefore, mark the area as
+                # containing an object.
+                out[1:3, y, x] = [0, 1]
+
+                # Compute the centre and width of the GT bbox.
+                bx0, bx1, by0, by1 = bbox
+                bx, by = (bx1 + bx0) / 2, (by1 + by0) / 2
+                bw, bh = bx1 - bx0, by1 - by0
+                del bbox, bx0, bx1, by0, by1
+
+                # Compute the bbox part of the label data.
+                lx, ly = bx - acx, by - acy
+                lw, lh = np.log(bw / a_width), np.log(bh / a_height)
+                out[3:, y, x] = [lx, ly, lw, lh]
+                del bx, by, bw, bh, lx, ly, lw, lh
+        return out
