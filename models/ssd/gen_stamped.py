773e2fa1a45c647fc7d05d5f54e77684a9e44294
Oliver Nagy <olitheolix@gmail.com>
2017-07-14 17:30:40 +1000

    Refactor: important parameters now available via arguments.

--- models/ssd/gen_stamped.py
+++ models/ssd/gen_stamped.py
@@ -6,18 +6,75 @@ into the image.
 
 """
 import os
+import tqdm
 import glob
 import pickle
-import tqdm
 import random
+import argparse
+import textwrap
 import numpy as np
 
 from PIL import Image
 
 
-def createForegroundShapes(N, width, height):
+def parseCmdline():
+    """Parse the command line arguments."""
+    description = textwrap.dedent('''\
+        Stamp foreground shapes into background images and record their BBoxes.
+
+        The background images will be loaded from "./data/background" and the
+        output images, along with the pickled BBox data will be written to
+        "./data/stamped".
+
+        The foreground shapes will be loaded from "./data/shapes". This folder
+        must contain specimen images for each shape in a separate sub-folder,
+        eg "./data/shapes/dog". Use `--cls-specimen` to limit the number of
+        images loaded for each label.
+
+        Use the `--dummy-shapes` option to ignore the folder and stamp only
+        discs and boxes into the image. This is mostly useful for debugging
+        because nets are easy to train for those two shapes.
+
+        Usage examples:
+          gen_stamped.py --width 32 --height 32 --num-stamps 100
+          gen_stamped.py --dummy-shapes
+    ''')
+
+    # Create a parser and program description.
+    parser = argparse.ArgumentParser(
+        description=description,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    padd = parser.add_argument
+
+    # Add the command line options.
+    padd('--N', metavar='', type=int, default=100,
+         help='Number of output images')
+    padd('--width', metavar='', type=int, default=64, help='Width (default 64)')
+    padd('--height', metavar='', type=int, default=64, help='Height (default 64)')
+    padd('--seed', metavar='', type=int, default=None,
+         help='Seed value for reproducible results (default None)')
+    padd('--num-stamps', metavar='', type=int, default=20,
+         help='Number of objects to embed in each background image (default 20)')
+    padd('--cls-specimen', metavar='', type=int, default=64,
+         help='Limit number of specimen images to load for each label')
+    padd('--dummy-shapes', action='store_true', default=False,
+         help='Use dummy shapes instead of loading them from disk')
+
+    # Parse the actual arguments.
+    param = parser.parse_args()
+    random.seed(param.seed)
+    np.random.seed(param.seed)
+    return param
+
+
+def createForegroundShapes(param):
     """Return hard coded 'square' and 'disc' shape."""
+    # The box and disk will fill the entire BBox minus this margin in pixels.
     margin = 2
+
+    # Convenience.
+    height, width = param.height, param.width
 
     # Box shape.
     box = np.zeros((height, width, 4), np.uint8)
@@ -39,7 +96,7 @@ def createForegroundShapes(N, width, height):
     # Replicate each shape N times. Give each shape a random colour.
     out = {}
     for name, shape in shapes.items():
-        img = np.array([shape] * N)
+        img = np.array([shape] * param.cls_specimen)
         for i in range(len(img)):
             img[i, :, :, :3] = img[i, :, :, :3] * np.random.uniform(0.3, 1, 3)
         out[name] = img
@@ -48,41 +105,45 @@ def createForegroundShapes(N, width, height):
     return out
 
 
-def loadForegroundShapes(N, width, height):
-    base = os.path.dirname(os.path.abspath(__file__))
-    base = os.path.join(base, 'data', 'shapes')
-    out = {}
+def loadForegroundShapes(path, param):
+    # Convenience.
+    height, width = param.height, param.width
 
+    # Makeshift threshold to remove images that are too dark. All images with
+    # less cumulative intensity will be considered too dark.
     thresh = 50 * width * height
 
     # Load cubes for all 10 labels.
+    out = {}
     for i in range(10):
         # Path to cubes with current label (ie number of side).
         name = f'{i:02d}'
-        path = os.path.join(base, name)
-        fnames = glob.glob(os.path.join(path, '*.jpg'))
+        fnames = glob.glob(os.path.join(path, name, '*.jpg'))
 
         # Load all cubes and assign an alpha map. Skip cubes that are too dark
         # as they have no chance of being identified.
         out[name] = []
-        for j, fname in enumerate(fnames[:N]):
-            # Load the image and convert to NumPy array.
+        for j, fname in enumerate(fnames):
+            # Load the image into a NumPy array.
             img = Image.open(fname).convert('RGBA')
             if img.size != (width, height):
                 img = img.resize((width, height))
             img = np.array(img, np.uint8)
 
-            # Crappy threshold to remove images that are too dark.
+            # Use makeshift threshold to remove images that are too dark.
             if np.sum(img[:, :, :3]) < thresh:
                 continue
 
             # Compute alpha mask. Basically, we _hope_ that dark areas are
             # outside the cube.
             intensity = np.sum(np.array(img[:, :, :3], np.float32), axis=2)
-            img[:, :, 3] = np.interp(intensity, [0, 20, 30, 255], [0, 25, 255, 255])
+            alpha = np.interp(intensity, [0, 20, 30, 255], [0, 25, 255, 255])
+            img[:, :, 3] = alpha
 
-            # Store the image.
+            # Add the image the list and abort the loop once we have enough images.
             out[name].append(img)
+            if len(out[name]) >= param.cls_specimen:
+                break
 
         # Convert the list of NumPy arrays to a single NumPy array.
         out[name] = np.array(out[name], np.uint8)
@@ -127,10 +188,10 @@ def stampImage(background, fg_shapes, N, xmin, xmax, ymin, ymax):
         attempts += 1
 
         # Compute random region in foreground image to put the object.
-        w = np.random.randint(xmin, xmax)
-        h = np.random.randint(ymin, ymax)
-        x0 = np.random.randint(0, bg_width - w)
-        y0 = np.random.randint(0, bg_height - h)
+        w = np.random.randint(xmin, 1 + xmax)
+        h = np.random.randint(ymin, 1 + ymax)
+        x0 = np.random.randint(0, 1 + bg_width - w)
+        y0 = np.random.randint(0, 1 + bg_height - h)
         x1, y1 = x0 + w, y0 + h
 
         # Verify if the region is already occupied. Do nothing if it is.
@@ -142,7 +203,7 @@ def stampImage(background, fg_shapes, N, xmin, xmax, ymin, ymax):
         # Pick a random foreground label and specimen.
         label = random.choice(list(fg_shapes.keys()))
         labels.append(label)
-        idx = np.random.randint(0, len(fg_shapes[label]) - 1)
+        idx = np.random.randint(0, len(fg_shapes[label]))
         fg = np.array(fg_shapes[label][idx])
 
         # Scale the foreground image.
@@ -159,7 +220,7 @@ def stampImage(background, fg_shapes, N, xmin, xmax, ymin, ymax):
     return out, bboxes, labels
 
 
-def generateImages(dst_path, bg_fnames, fg_shapes, int2name, num_img, num_stamps):
+def generateImages(dst_path, param, bg_fnames, fg_shapes, int2name):
     """Create N stamped background images and save them."""
     xmax = max([_.shape[2] for _ in fg_shapes.values()])
     ymax = max([_.shape[1] for _ in fg_shapes.values()])
@@ -167,13 +228,13 @@ def generateImages(dst_path, bg_fnames, fg_shapes, int2name, num_img, num_stamps
     dims = (xmin, xmax, ymin, ymax)
 
     # Create N images.
-    for i in tqdm.tqdm(range(num_img)):
+    for i in tqdm.tqdm(range(param.N)):
         # Load background image as NumPy array.
         img = Image.open(bg_fnames[i % len(bg_fnames)]).convert('RGB')
         img = np.array(img, np.uint8)
 
         # Stamp foreground shapes into background image.
-        img, bboxes, labels = stampImage(img, fg_shapes, num_stamps, *dims)
+        img, bboxes, labels = stampImage(img, fg_shapes, param.num_stamps, *dims)
 
         # File name prefix for image and meta data.
         fname = os.path.join(dst_path, f'{i:04d}')
@@ -187,11 +248,14 @@ def generateImages(dst_path, bg_fnames, fg_shapes, int2name, num_img, num_stamps
 
 
 def main():
+    param = parseCmdline()
+
     # Folders with background images and for output images.
     base = os.path.dirname(os.path.abspath(__file__))
     base = os.path.join(base, 'data')
     src_path = os.path.join(base, 'background')
     dst_path = os.path.join(base, 'stamped')
+    shape_path = os.path.join(base, 'shapes')
 
     # Ensure output path exists.
     os.makedirs(dst_path, exist_ok=True)
@@ -201,17 +265,19 @@ def main():
     assert len(bg_fnames) > 0
 
     # Load the foreground objects.
-    shapes = createForegroundShapes(N=10, width=32, height=32)
-    shapes = loadForegroundShapes(N=10, width=32, height=32)
+    if param.dummy_shapes:
+        shapes = createForegroundShapes(param)
+    else:
+        shapes = loadForegroundShapes(shape_path, param)
 
     # Compile a map to convert a label ID to its human readable name. Note that
-    # this is 1-based because label 0 will always be reserved for the
-    # background (ie no-object) label in later stages.
+    # this is 1-based because label 0 is reserved for "background" (ie
+    # no-object) label in later stages.
     int2name = {idx + 1: name for idx, name in enumerate(shapes)}
     int2name[0] = 'background'
 
     # Stamp the foreground objects into the background images.
-    generateImages(dst_path, bg_fnames, shapes, int2name, num_img=10, num_stamps=20)
+    generateImages(dst_path, param, bg_fnames, shapes, int2name)
 
 
 if __name__ == '__main__':
