0da93846509d52f3c595307fe85ea4f50daddf7a
Oliver Nagy <olitheolix@gmail.com>
2017-07-19 08:35:42 +1000

    Added num_pools_{shared,rpn}

--- models/ssd/config.py
+++ models/ssd/config.py
@@ -3,6 +3,7 @@ import collections
 
 NetConf = collections.namedtuple(
     'NetConf',
-    'seed width height colour dtype keep_prob '
-    'train_rat num_epochs num_samples path'
+    'seed width height colour dtype keep_prob'
+    ' num_pools_shared num_pools_rpn'
+    ' train_rat num_epochs num_samples path'
 )
