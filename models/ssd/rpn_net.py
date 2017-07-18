660602620315330acd7e03d86ba42f20a15419c3
Oliver Nagy <olitheolix@gmail.com>
2017-07-19 08:47:25 +1000

    Overhauled and simplified the RPN cost.

--- models/ssd/rpn_net.py
+++ models/ssd/rpn_net.py
@@ -21,72 +21,78 @@ def model(x_in, layer_id, bwt1, bwt2):
         b2 = tf.Variable(bwt2[0], trainable=bwt2[2], name='b2')
         W2 = tf.Variable(bwt2[1], trainable=bwt2[2], name='W2')
 
-def cost(pred_net, gt_net, num_classes, mask_cls, mask_bbox):
-    """ Return the scalar cost node.
+        rpn_out = tf.nn.conv2d(net_out, W2, [1, 1, 1, 1], **conv_opts)
+        rpn_out = tf.add(rpn_out, b2, name='rpn_out')
+    return net_out, rpn_out
 
-    Examples assume that gt_net.shape == (N, 5, 128, 128) and that we have 10
-    classes including background.
-    """
-    assert mask_cls.shape[1:] == mask_bbox.shape[1:]
-    height, width = mask_bbox.shape.as_list()[1:]
-    assert gt_net.shape.as_list()[1:] == [4 + num_classes, height, width]
-    assert pred_net.shape.as_list()[1:] == [4 + num_classes, height, width]
 
+def cost(net_id, pred_net):
+    """ Return the scalar cost node."""
+    chan, ft_height, ft_width = pred_net.shape.as_list()[1:]
+
+    num_classes = chan - 4
+    assert num_classes > 1
+
+    dtype = pred_net.dtype
     cost_ce = tf.nn.softmax_cross_entropy_with_logits
-    with tf.variable_scope('rpn-cost'):
+    with tf.variable_scope(f'rpn{net_id}-cost'):
+        y_in = tf.placeholder(dtype, pred_net.shape, name='y')
+        mask_cls = tf.placeholder(dtype, [None, ft_height, ft_width], name='mask_cls')
+        mask_bbox = tf.placeholder(dtype, [None, ft_height, ft_width], name='mask_bbox')
+
         # It will be more convenient to have the image dimensions first.
         # In:  [N, 4 + num_classes, 128, 128]
         # Out: [N, 128, 128, 4 + num_classes]
-        gt_net = tf.transpose(gt_net, [0, 2, 3, 1])
+        gt_net = tf.transpose(y_in, [0, 2, 3, 1])
         pred_net = tf.transpose(pred_net, [0, 2, 3, 1])
-        assert gt_net.shape.as_list()[1:] == [height, width, 4 + num_classes]
-        assert pred_net.shape.as_list()[1:] == [height, width, 4 + num_classes]
+        assert gt_net.shape.as_list()[1:] == [ft_height, ft_width, 4 + num_classes]
+        assert pred_net.shape.as_list()[1:] == [ft_height, ft_width, 4 + num_classes]
 
         # Unpack the four BBox parameters.
         # In:  [N, 128, 128, 4 + num_classes]
         # Out: [N, 128, 128, 4]
         gt_bbox = tf.slice(gt_net, [0, 0, 0, 0], [-1, -1, -1, 4], name='gt_bbox')
         pred_bbox = tf.slice(pred_net, [0, 0, 0, 0], [-1, -1, -1, 4], name='pred_bbox')
-        assert gt_bbox.shape.as_list()[1:] == [height, width, 4]
-        assert pred_bbox.shape.as_list()[1:] == [height, width, 4]
+        assert gt_bbox.shape.as_list()[1:] == [ft_height, ft_width, 4]
+        assert pred_bbox.shape.as_list()[1:] == [ft_height, ft_width, 4]
 
         # Unpack one-hot encoded class labels.
         # In:  [N, 128, 128, 4 + num_classes]
         # Out: [N, 128, 128, num_classes]
         gt_cls = tf.slice(gt_net, [0, 0, 0, 4], [-1, -1, -1, -1], name='gt_cls')
         pred_cls = tf.slice(pred_net, [0, 0, 0, 4], [-1, -1, -1, -1], name='pred_cls')
-        assert gt_cls.shape.as_list()[1:] == [height, width, num_classes]
-        assert pred_cls.shape.as_list()[1:] == [height, width, num_classes]
+        assert gt_cls.shape.as_list()[1:] == [ft_height, ft_width, num_classes]
+        assert pred_cls.shape.as_list()[1:] == [ft_height, ft_width, num_classes]
 
         # Cost function for class label
         # In:  [N, 128, 128, num_classes]
         # Out: [N, 128, 128]
         cost_cls = cost_ce(logits=pred_cls, labels=gt_cls)
-        assert cost_cls.shape.as_list()[1:] == [height, width]
+        assert cost_cls.shape.as_list()[1:] == [ft_height, ft_width]
         del gt_cls, pred_cls
 
         # Cost function for the four BBox parameters.
         # In:  [N, 128, 128, 4]
         # Out: [N, 128, 128, 4]
         cost_bbox = tf.abs(gt_bbox - pred_bbox)
-        assert gt_bbox.shape.as_list()[1:] == [128, 128, 4]
-        assert pred_bbox.shape.as_list()[1:] == [128, 128, 4]
-        assert cost_bbox.shape.as_list()[1:] == [128, 128, 4], cost_cls.shape
+        assert gt_bbox.shape.as_list()[1:] == [ft_height, ft_width, 4]
+        assert pred_bbox.shape.as_list()[1:] == [ft_height, ft_width, 4]
+        assert cost_bbox.shape.as_list()[1:] == [ft_height, ft_width, 4]
         del gt_bbox, pred_bbox
 
         # Average the BBox cost over the 4 parameters.
         # In:  [N, 128, 128, 4]
         # Out: [N, 128, 128]
         cost_bbox = tf.reduce_mean(cost_bbox, axis=3, keep_dims=False)
-        assert cost_bbox.shape.as_list()[1:] == [128, 128], cost_bbox.shape
+        assert cost_bbox.shape.as_list()[1:] == [ft_height, ft_width], cost_bbox.shape
 
         # Only retain the cost at the mask locations.
         # In:  [N, 128, 128]
         # Out: [N, 128, 128]
         cost_cls = tf.multiply(cost_cls, mask_cls)
         cost_bbox = tf.multiply(cost_bbox, mask_bbox)
-        assert cost_cls.shape.as_list()[1:] == [128, 128]
-        assert cost_bbox.shape.as_list()[1:] == [128, 128]
+        assert cost_cls.shape.as_list()[1:] == [ft_height, ft_width]
+        assert cost_bbox.shape.as_list()[1:] == [ft_height, ft_width]
 
         # Compute a single scalar cost.
         # In:  [N, 128, 128]
