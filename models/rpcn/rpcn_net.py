f3009831cfb477d350c57ae2880b08d94031b0fb
Oliver Nagy <olitheolix@gmail.com>
2017-07-27 20:57:42 +1000

    Changed feature tensor layout and cost function in model.

--- models/rpcn/rpcn_net.py
+++ models/rpcn/rpcn_net.py
@@ -36,84 +36,64 @@ def model(x_in, name, bwt1, bwt2):
     return net_out, rpcn_out
 
 
-def cost(rpcn_dim):
-    """ Return the scalar cost node."""
-    assert len(rpcn_dim) == 2
-    assert isinstance(rpcn_dim[0], int) and isinstance(rpcn_dim[1], int)
-    g = tf.get_default_graph().get_tensor_by_name
-    name = f'{rpcn_dim[0]}x{rpcn_dim[1]}'
-    pred_net = g(f'rpcn-{name}/rpcn_out:0')
-    chan, ft_height, ft_width = pred_net.shape.as_list()[1:]
+def _crossEnt(logits, labels, name=None):
+    ce = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels)
+    return tf.add(ce, 0, name=name)
 
-    num_classes = chan - 4
-    assert num_classes > 1
 
-    dtype = pred_net.dtype
-    cost_ce = tf.nn.softmax_cross_entropy_with_logits
+def cost(y_pred):
+    dtype = y_pred.dtype
+    chan, ft_height, ft_width = y_pred.shape.as_list()[1:]
+    mask_dim = (ft_height, ft_width)
+    name = f'{ft_height}x{ft_width}'
+    del chan, ft_height, ft_width
+
     with tf.variable_scope(f'rpcn-{name}-cost'):
-        y_in = tf.placeholder(dtype, pred_net.shape, name='y')
-        mask_cls = tf.placeholder(dtype, [None, ft_height, ft_width], name='mask_cls')
-        mask_bbox = tf.placeholder(dtype, [None, ft_height, ft_width], name='mask_bbox')
+        # Placeholder for ground truth data.
+        y_true = tf.placeholder(dtype, y_pred.shape, name='y_true')
 
-        # It will be more convenient to have the image dimensions first.
-        # In:  [N, 4 + num_classes, 128, 128]
-        # Out: [N, 128, 128, 4 + num_classes]
-        gt_net = tf.transpose(y_in, [0, 2, 3, 1])
-        pred_net = tf.transpose(pred_net, [0, 2, 3, 1])
-        assert gt_net.shape.as_list()[1:] == [ft_height, ft_width, 4 + num_classes]
-        assert pred_net.shape.as_list()[1:] == [ft_height, ft_width, 4 + num_classes]
+        # Masks matrices. Each one has shape [128, 128]
+        mask_bbox = tf.placeholder(dtype, mask_dim, name='mask_bbox')
+        mask_isFg = tf.placeholder(dtype, mask_dim, name='mask_isFg')
+        mask_cls = tf.placeholder(dtype, mask_dim, name='mask_cls')
 
-        # Unpack the four BBox parameters.
-        # In:  [N, 128, 128, 4 + num_classes]
-        # Out: [N, 128, 128, 4]
-        gt_bbox = tf.slice(gt_net, [0, 0, 0, 0], [-1, -1, -1, 4], name='gt_bbox')
-        pred_bbox = tf.slice(pred_net, [0, 0, 0, 0], [-1, -1, -1, 4], name='pred_bbox')
-        assert gt_bbox.shape.as_list()[1:] == [ft_height, ft_width, 4]
-        assert pred_bbox.shape.as_list()[1:] == [ft_height, ft_width, 4]
+        # It will be more convenient to have the data dimension last.
+        # In:  [1, *, 128, 128]
+        # Out: [1, 128, 128, *]
+        yt = tf.transpose(y_true, [0, 2, 3, 1])
+        yp = tf.transpose(y_pred, [0, 2, 3, 1])
 
-        # Unpack one-hot encoded class labels.
-        # In:  [N, 128, 128, 4 + num_classes]
-        # Out: [N, 128, 128, num_classes]
-        gt_cls = tf.slice(gt_net, [0, 0, 0, 4], [-1, -1, -1, -1], name='gt_cls')
-        pred_cls = tf.slice(pred_net, [0, 0, 0, 4], [-1, -1, -1, -1], name='pred_cls')
-        assert gt_cls.shape.as_list()[1:] == [ft_height, ft_width, num_classes]
-        assert pred_cls.shape.as_list()[1:] == [ft_height, ft_width, num_classes]
+        # Unpack the tensor portions for the BBox, is-foreground, and labels.
+        yp_bbox = tf.slice(yp, (0, 0, 0, 0), (-1, -1, -1, 4))
+        yt_bbox = tf.slice(yt, (0, 0, 0, 0), (-1, -1, -1, 4))
+        yp_isFg = tf.slice(yp, (0, 0, 0, 4), (-1, -1, -1, 2))
+        yt_isFg = tf.slice(yt, (0, 0, 0, 4), (-1, -1, -1, 2))
+        yp_cls = tf.slice(yp, (0, 0, 0, 6), (-1, -1, -1, -1))
+        yt_cls = tf.slice(yt, (0, 0, 0, 6), (-1, -1, -1, -1))
 
-        # Cost function for class label
-        # In:  [N, 128, 128, num_classes]
-        # Out: [N, 128, 128]
-        cost_cls = cost_ce(logits=pred_cls, labels=gt_cls)
-        assert cost_cls.shape.as_list()[1:] == [ft_height, ft_width]
-        del gt_cls, pred_cls
+        # Compute the costs for all constituent components.
+        # In:  [1, 128, 128, *]
+        # Out: [1, 128, 128]
+        ce_bbox = tf.reduce_sum(tf.abs(yp_bbox - yt_bbox), axis=3, name='bbox_full')
+        ce_isFg = _crossEnt(logits=yp_isFg, labels=yt_isFg, name='isFg_full')
+        ce_cls = _crossEnt(logits=yp_cls, labels=yt_cls, name='cls_full')
 
-        # Cost function for the four BBox parameters.
-        # In:  [N, 128, 128, 4]
-        # Out: [N, 128, 128, 4]
-        cost_bbox = tf.abs(gt_bbox - pred_bbox)
-        assert gt_bbox.shape.as_list()[1:] == [ft_height, ft_width, 4]
-        assert pred_bbox.shape.as_list()[1:] == [ft_height, ft_width, 4]
-        assert cost_bbox.shape.as_list()[1:] == [ft_height, ft_width, 4]
-        del gt_bbox, pred_bbox
+        # Void the cost wherever the respective masks are zero.
+        # In:  [1, 128, 128]
+        # Out: [1, 128, 128]
+        cost_bbox = tf.multiply(ce_bbox, mask_bbox)
+        cost_isFg = tf.multiply(ce_isFg, mask_isFg)
+        cost_cls = tf.multiply(ce_cls, mask_cls)
 
-        # Average the BBox cost over the 4 parameters.
-        # In:  [N, 128, 128, 4]
-        # Out: [N, 128, 128]
-        cost_bbox = tf.reduce_mean(cost_bbox, axis=3, keep_dims=False)
-        assert cost_bbox.shape.as_list()[1:] == [ft_height, ft_width], cost_bbox.shape
+        # Reduce all cost tensors to cost scalars.
+        # In:  [1, 128, 128]
+        # Out: [1]
+        cost_bbox = tf.reduce_mean(cost_bbox, name='bbox')
+        cost_isFg = tf.reduce_mean(cost_isFg, name='isFg')
+        cost_cls = tf.reduce_mean(cost_cls, name='cls')
 
-        # Only retain the cost at the mask locations.
-        # In:  [N, 128, 128]
-        # Out: [N, 128, 128]
-        cost_cls = tf.multiply(cost_cls, mask_cls)
-        cost_bbox = tf.multiply(cost_bbox, mask_bbox)
-        assert cost_cls.shape.as_list()[1:] == [ft_height, ft_width]
-        assert cost_bbox.shape.as_list()[1:] == [ft_height, ft_width]
-
-        # Compute a single scalar cost.
-        # In:  [N, 128, 128]
-        # Out: [N]
-        cost_tot = tf.reduce_sum(cost_cls + cost_bbox, name='cost')
-    return cost_tot
+        # Compute final scalar cost.
+        tf.add_n([cost_bbox, cost_isFg, cost_cls], name='total')
 
 
 def save(fname, sess, ft_out_dims):
---
+++ models/rpcn/test_rpcn.py
@@ -0,0 +1,319 @@
+import rpcn_net
+import numpy as np
+import tensorflow as tf
+import feature_compiler
+
+setBBoxRects = feature_compiler.setBBoxRects
+getBBoxRects = feature_compiler.getBBoxRects
+setIsFg = feature_compiler.setIsFg
+getIsFg = feature_compiler.getIsFg
+setClassLabel = feature_compiler.setClassLabel
+getClassLabel = feature_compiler.getClassLabel
+
+
+class TestCost:
+    """Test the various cost components."""
+    @classmethod
+    def setup_class(cls):
+        # Size of last layer and number of unique classes.
+        cls.ft_dim = (2, 2)
+        cls.num_classes = 10
+
+        # Feature tensor.
+        out_dim = (1, 4 + 2 + cls.num_classes, *cls.ft_dim)
+        cls.y_pred_in = tf.placeholder(tf.float32, out_dim, name='y_pred')
+
+        # Setup cost computation. This will create a node for `y_true`.
+        rpcn_net.cost(cls.y_pred_in)
+
+        # Get the placeholder for the true input (see above).
+        g = tf.get_default_graph().get_tensor_by_name
+        cls.y_true_in = g('rpcn-2x2-cost/y_true:0')
+
+        # Create Tensorflow session.
+        cls.sess = tf.Session()
+
+    @classmethod
+    def teardown_class(cls):
+        # Shutdown Tensorflow session and reset graph.
+        cls.sess.close()
+        tf.reset_default_graph()
+
+    def setup_method(self, method):
+        pass
+
+    def teardown_method(self, method):
+        pass
+
+    def crossEnt(self, logits, labels):
+        """Return cross entropy.
+
+        Unlike the Tensorflow equivalent, this one reduces over the first
+        dimension, instead of over the last.
+        """
+        assert logits.shape == labels.shape
+        sm = np.exp(logits) / np.sum(np.exp(logits), axis=0)
+        return -np.sum(labels * np.log(sm), axis=0)
+
+    def test_myCrossEntropy(self):
+        CE = tf.nn.softmax_cross_entropy_with_logits
+
+        # Vector.
+        logits = np.array([0.1, 2], np.float32)
+        labels = np.array([1, 0], np.float32)
+        ce_np = self.crossEnt(logits, labels)
+        ce_tf = self.sess.run(CE(logits=logits, labels=labels, dim=0))
+        assert ce_np.shape == ce_tf.shape
+        assert np.allclose(ce_np, ce_tf, 0, 1E-4)
+
+        # Matrix.
+        logits = np.array([[0.1, 1], [0.2, 2]], np.float32)
+        labels = np.array([[1, 0], [0, 1]], np.float32)
+        ce_np = self.crossEnt(logits, labels)
+        ce_tf = self.sess.run(CE(logits=logits, labels=labels, dim=0))
+        assert ce_np.shape == ce_tf.shape
+        assert np.allclose(ce_np, ce_tf, 0, 1E-4)
+
+        # 4D Tensor.
+        logits = np.random.uniform(-1, 2, (1, 4, 4, 2))
+        labels = np.random.randint(0, 2, logits.shape)
+        labels[:, :, :, 1] = 1 - labels[:, :, :, 0]
+        ce_np = self.crossEnt(logits, labels)
+        ce_tf = self.sess.run(CE(logits=logits, labels=labels, dim=0))
+        assert ce_np.shape == ce_tf.shape
+        assert np.allclose(ce_np, ce_tf, 0, 1E-4)
+
+    def test_basic_nodes(self):
+        """Check that all nodes that should exist do exist."""
+        g = tf.get_default_graph().get_tensor_by_name
+        y_true_in = self.y_true_in
+        y_pred_in = self.y_pred_in
+
+        cost_isFg = g('rpcn-2x2-cost/isFg:0')
+        cost_isFg_full = g('rpcn-2x2-cost/isFg_full:0')
+        mask_isFg_in = g('rpcn-2x2-cost/mask_isFg:0')
+
+        cost_cls = g('rpcn-2x2-cost/cls:0')
+        cost_cls_full = g('rpcn-2x2-cost/cls_full:0')
+        mask_cls_in = g('rpcn-2x2-cost/mask_cls:0')
+
+        cost_bbox = g('rpcn-2x2-cost/bbox:0')
+        cost_bbox_full = g('rpcn-2x2-cost/bbox_full:0')
+        mask_bbox_in = g('rpcn-2x2-cost/mask_bbox:0')
+
+        cost_total = g('rpcn-2x2-cost/total:0')
+        assert cost_total.shape == tuple()
+
+        assert y_true_in.dtype == y_pred_in.dtype == tf.float32
+        assert y_pred_in.shape == [1, 2 + 4 + self.num_classes, *self.ft_dim]
+        assert y_pred_in.shape == y_true_in.shape
+
+        assert mask_isFg_in.shape == self.ft_dim
+        assert mask_isFg_in.dtype == tf.float32
+        assert cost_isFg.dtype == cost_isFg_full.dtype == tf.float32
+        assert cost_isFg_full.shape == [1, *self.ft_dim]
+
+        assert mask_cls_in.shape == self.ft_dim
+        assert mask_cls_in.dtype == tf.float32
+        assert cost_cls.dtype == cost_cls_full.dtype == tf.float32
+        assert cost_cls_full.shape == [1, *self.ft_dim]
+
+        assert mask_bbox_in.shape == self.ft_dim
+        assert mask_bbox_in.dtype == tf.float32
+        assert cost_bbox.dtype == cost_cls_full.dtype == tf.float32
+        assert cost_bbox_full.shape == [1, *self.ft_dim]
+
+    def _checkIsForegroundCost(self, y_pred, y_true, mask):
+        """Compute the cost with NumPy and compare against Tensorflow.
+
+        The cost function for the is-foreground label is the cross-entropy.
+        """
+        g = tf.get_default_graph().get_tensor_by_name
+        cost = g('rpcn-2x2-cost/isFg:0')
+        cost_full = g('rpcn-2x2-cost/isFg_full:0')
+        assert cost_full.shape == (1, *self.ft_dim)
+
+        mask_in = g('rpcn-2x2-cost/mask_isFg:0')
+        fd = {self.y_pred_in: y_pred, self.y_true_in: y_true, mask_in: mask}
+        out_full, out = self.sess.run([cost_full, cost], feed_dict=fd)
+
+        # Remove the (unused) batch dimension.
+        out_full = out_full[0]
+
+        # Compute expected cost value with NumPy and compare.
+        ref = self.crossEnt(getIsFg(y_pred), getIsFg(y_true))
+        assert np.allclose(out_full, ref, 0, 1E-4)
+        assert np.allclose(np.mean(ref * mask), out)
+        return out
+
+    def test_cost_isForeground(self):
+        """Cost function for binary is-foreground label."""
+        # Activate two locations in 2x2 mask.
+        mask = np.zeros(self.ft_dim, np.float32)
+        mask[0, 0] = mask[1, 1] = 1
+
+        # Allocate output tensor. We will fill it with test values below.
+        y_pred = np.zeros(self.y_true_in.shape, np.float32)
+
+        # Create an is-foreground feature with two locations marked as
+        # foreground and background, respectively.
+        fg = np.array([[0, 1], [1, 0]], y_pred.dtype)
+        cls_fg = np.array([fg, 1 - fg])
+        assert cls_fg.shape == (2, *self.ft_dim)
+
+        # Perfect estimate: the true and predicted labels match.
+        y_pred = setIsFg(y_pred, cls_fg)
+        y_true = np.array(y_pred)
+        self._checkIsForegroundCost(y_pred, y_true, mask)
+
+        # Imperfect estimate: the predicted labels are random.
+        dim = (2, *self.ft_dim)
+        y_pred = setIsFg(y_pred, np.random.uniform(-1, 2, dim))
+        self._checkIsForegroundCost(y_pred, y_true, mask)
+
+    def _checkIsClassCost(self, y_pred, y_true, mask):
+        """Compute the cost with NumPy and compare against Tensorflow.
+
+        The cost function for the is-foreground label is the cross-entropy.
+        """
+        g = tf.get_default_graph().get_tensor_by_name
+        cost = g('rpcn-2x2-cost/cls:0')
+        cost_full = g('rpcn-2x2-cost/cls_full:0')
+        assert cost_full.shape == (1, *self.ft_dim)
+
+        mask_in = g('rpcn-2x2-cost/mask_cls:0')
+        fd = {self.y_pred_in: y_pred, self.y_true_in: y_true, mask_in: mask}
+        out_full, out = self.sess.run([cost_full, cost], feed_dict=fd)
+
+        # Remove the (unused) batch dimension.
+        out_full = out_full[0]
+
+        # Compute expected cost value with NumPy and compare.
+        ref = self.crossEnt(getClassLabel(y_pred), getClassLabel(y_true))
+        assert np.allclose(out_full, ref, 0, 1E-4)
+        assert np.allclose(np.mean(ref * mask), out)
+        return out
+
+    def test_cost_classLabels(self):
+        """Cost function for the num_classes possible class labels."""
+        # Activate two locations in 2x2 mask.
+        mask = np.zeros(self.ft_dim, np.float32)
+        mask[0, 0] = mask[1, 1] = 1
+
+        # Allocate output tensor. We will fill it with test values below.
+        y_pred = np.zeros(self.y_true_in.shape, np.float32)
+
+        # Create a random class label for each location.
+        dim = (self.num_classes, *self.ft_dim)
+        cls_labels = np.random.randint(0, self.num_classes, dim)
+
+        # Perfect estimate: the true and predicted labels match.
+        y_pred = setClassLabel(y_pred, cls_labels)
+        y_true = np.array(y_pred)
+        self._checkIsClassCost(y_pred, y_true, mask)
+
+        # Imperfect estimate: the predicted labels are random.
+        y_pred = setClassLabel(y_pred, np.random.uniform(-1, 2, dim))
+        self._checkIsClassCost(y_pred, y_true, mask)
+
+    def _checkBBoxCost(self, y_pred, y_true, mask):
+        """Compute the cost with NumPy and compare against Tensorflow.
+
+        The cost function computes the L1 error at each location and sums
+        it over the 4 BBox coordinates. In other words, the input BBox
+        tensor has shape [4, height, width] and the output [height, width].
+        """
+        g = tf.get_default_graph().get_tensor_by_name
+        cost = g('rpcn-2x2-cost/bbox:0')
+        cost_full = g('rpcn-2x2-cost/bbox_full:0')
+        assert cost_full.shape == (1, *self.ft_dim)
+
+        mask_in = g('rpcn-2x2-cost/mask_bbox:0')
+        fd = {self.y_pred_in: y_pred, self.y_true_in: y_true, mask_in: mask}
+        out_full, out = self.sess.run([cost_full, cost], feed_dict=fd)
+
+        # Remove the (unused) batch dimension.
+        out_full = out_full[0]
+
+        # Compute expected cost value with NumPy and compare.
+        ref = np.abs(getBBoxRects(y_pred) - getBBoxRects(y_true))
+        ref = np.sum(ref, axis=0)
+        assert np.allclose(out_full, ref, 0, 1E-4)
+        assert np.allclose(np.mean(ref * mask), out)
+        return out
+
+    def test_cost_BBox(self):
+        """Cost function for the num_classes possible class labels."""
+        # Activate two locations in 2x2 mask.
+        mask = np.zeros(self.ft_dim, np.float32)
+        mask[0, 0] = mask[1, 1] = 1
+
+        # Allocate output tensor. We will fill it with test values below.
+        y_pred = np.zeros(self.y_true_in.shape, np.float32)
+
+        # Create random BBox parameters for each location.
+        dim = (4, *self.ft_dim)
+        bbox_rects = np.random.uniform(0, 512, dim)
+
+        # Perfect estimate: the true and predicted labels match.
+        y_pred = setBBoxRects(y_pred, bbox_rects)
+        y_true = np.array(y_pred)
+        self._checkBBoxCost(y_pred, y_true, mask)
+
+        # Imperfect estimate: the predicted BBox corners are random.
+        y_pred = setBBoxRects(y_pred, np.random.uniform(0, 512, dim))
+        self._checkBBoxCost(y_pred, y_true, mask)
+
+    def test_total_cost(self):
+        """Final cost function to minimise"""
+        # Allocate output tensor. We will fill it with test values below.
+        y_pred = np.zeros(self.y_true_in.shape, np.float32)
+        y_true = np.zeros_like(y_pred)
+
+        # Convenience
+        num_cls = self.num_classes
+
+        # Test several random inputs.
+        for i in range(10):
+            # Create three random masks.
+            mask_isFg = np.random.randint(0, 2, self.ft_dim)
+            mask_bbox = np.random.randint(0, 2, self.ft_dim)
+            mask_cls = np.random.randint(0, 2, self.ft_dim)
+
+            # Create random network output.
+            cls_fg = np.random.randint(0, 2, (2, *self.ft_dim))
+            cls_labels = np.random.randint(0, num_cls, (num_cls, *self.ft_dim))
+            bbox_rects = np.random.uniform(0, 512, (4, *self.ft_dim))
+            y_pred = setIsFg(y_pred, cls_fg)
+            y_pred = setClassLabel(y_pred, cls_labels)
+            y_pred = setBBoxRects(y_pred, bbox_rects)
+
+            # Create random ground truth.
+            cls_fg = np.random.randint(0, 2, (2, *self.ft_dim))
+            cls_labels = np.random.randint(0, num_cls, (num_cls, *self.ft_dim))
+            bbox_rects = np.random.uniform(0, 512, (4, *self.ft_dim))
+            y_true = setIsFg(y_pred, cls_fg)
+            y_true = setClassLabel(y_pred, cls_labels)
+            y_true = setBBoxRects(y_pred, bbox_rects)
+
+            # Verify the constituent costs.
+            c0 = self._checkBBoxCost(y_pred, y_true, mask_bbox)
+            c1 = self._checkIsClassCost(y_pred, y_true, mask_cls)
+            c2 = self._checkIsForegroundCost(y_pred, y_true, mask_isFg)
+
+            # Compute the total cost with NumPy.
+            np_cost = c0 + c1 + c2
+
+            # Compute the total cost via Tensorflow.
+            g = tf.get_default_graph().get_tensor_by_name
+            cost = g('rpcn-2x2-cost/total:0')
+            fd = {
+                self.y_pred_in: y_pred, self.y_true_in: y_true,
+                g('rpcn-2x2-cost/mask_isFg:0'): mask_isFg,
+                g('rpcn-2x2-cost/mask_cls:0'): mask_cls,
+                g('rpcn-2x2-cost/mask_bbox:0'): mask_bbox,
+            }
+            tf_cost = self.sess.run(cost, feed_dict=fd)
+
+            # Ensure Tensorflow and NumPy agree.
+            assert np.abs(np_cost - tf_cost) < 1E-3
