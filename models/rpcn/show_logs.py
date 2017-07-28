961cbbec053024702cec93d076b1a35f0049fcec
Oliver Nagy <olitheolix@gmail.com>
2017-07-28 20:37:29 +1000

    Overhauled show_logs to work with new feature tensor format.

--- models/rpcn/show_logs.py
+++ models/rpcn/show_logs.py
@@ -5,16 +5,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def computePercentile(vec, percentile):
+    assert 0 <= percentile <= 100
+    N = len(vec) - 1
+    N = int(N * percentile / 100)
+    vec = np.sort(vec)
+    return vec[N]
+
+
 def compileStatistics(layer_log, num_epochs, samples_per_epoch):
-    bb_err_all = [_.bbox_err for _ in layer_log['acc']]
+    bb_err_all = [_.bbox for _ in layer_log['err']]
     assert len(bb_err_all) == num_epochs * samples_per_epoch
 
     data = {}
-    for percentile in [50, 75, 90, 95, 99]:
+    for percentile in [50, 75, 90, 95, 99, 100]:
         pstr = f'{percentile}p'
         data[pstr] = {
-            'cost': np.zeros(num_epochs, np.float32),
-            'fg_err': np.zeros(num_epochs, np.float32),
+            'cost_bbox': np.zeros(num_epochs, np.float32),
+            'cost_isFg': np.zeros(num_epochs, np.float32),
+            'cost_cls': np.zeros(num_epochs, np.float32),
+            'bgfg_err': np.zeros(num_epochs, np.float32),
+            'label_err': np.zeros(num_epochs, np.float32),
             'fg_falsepos': np.zeros(num_epochs, np.float32),
             'bg_falsepos': np.zeros(num_epochs, np.float32),
             'bb_err_x0': np.zeros(num_epochs, np.float32),
@@ -28,59 +39,69 @@ def compileStatistics(layer_log, num_epochs, samples_per_epoch):
         for epoch in range(num_epochs):
             start = epoch * samples_per_epoch
             stop = start + samples_per_epoch
-            acc = layer_log['acc'][start:stop]
-            cost = np.array(layer_log['cost'][start:stop])
-            fg_err = np.array([_.fgcls_err for _ in acc])
-            true_fg_tot = np.array([_.true_fg_tot for _ in acc])
-            true_bg_tot = np.array([_.true_bg_tot for _ in acc])
-            bg_falsepos = np.array([_.pred_bg_falsepos for _ in acc])
-            fg_falsepos = np.array([_.pred_fg_falsepos for _ in acc])
+            err = layer_log['err'][start:stop]
+
+            cost = layer_log['cost'][start:stop]
+            cost_bbox = np.array([_['bbox'] for _ in cost])
+            cost_isFg = np.array([_['isFg'] for _ in cost])
+            cost_cls = np.array([_['cls'] for _ in cost])
+            del cost
+
+            bgfg = np.array([_.BgFg for _ in err])
+            label = np.array([_.label for _ in err])
+            num_BgFg = np.array([_.num_BgFg for _ in err])
+            num_labels = np.array([_.num_labels for _ in err])
+            num_fg = np.array([_.num_Fg for _ in err])
+            num_bg = np.array([_.num_Bg for _ in err])
+            falsepos_bg = np.array([_.falsepos_bg for _ in err])
+            falsepos_fg = np.array([_.falsepos_fg for _ in err])
             del start, stop
 
             # These will be used to compute percentages and may lead to
             # division-by-zero errors.
-            true_fg_tot = np.clip(true_fg_tot, 1, None)
-            true_bg_tot = np.clip(true_bg_tot, 1, None)
+            num_fg = np.clip(num_fg, 1, None)
+            num_bg = np.clip(num_bg, 1, None)
+            num_BgFg = np.clip(num_BgFg, 1, None)
+            num_labels = np.clip(num_labels, 1, None)
 
             # Cost.
-            assert cost.ndim == 1
-            cost = np.sort(cost)
-            cost = cost[int(len(cost) * (percentile / 100))]
-            d['cost'][epoch] = cost
-            del cost
+            assert cost_bbox.ndim == cost_isFg.ndim == cost_cls.ndim == 1
+            d['cost_bbox'][epoch] = computePercentile(cost_bbox, percentile)
+            d['cost_isFg'][epoch] = computePercentile(cost_isFg, percentile)
+            d['cost_cls'][epoch] = computePercentile(cost_cls, percentile)
+            del cost_bbox, cost_isFg, cost_cls
 
-            # Class accuracy for foreground shapes.
-            fg_err = np.sort(100 * fg_err / true_fg_tot)
-            fg_err = fg_err[int(len(fg_err) * (percentile / 100))]
-            d['fg_err'][epoch] = fg_err
-            del fg_err
+            # Class accuracy for foreground/background distinction.
+            bgfg_err = 100 * bgfg / num_BgFg
+            d['bgfg_err'][epoch] = computePercentile(bgfg_err, percentile)
+            del bgfg_err
+
+            # Class accuracy for foreground label.
+            label_err = 100 * label / num_labels
+            d['label_err'][epoch] = computePercentile(label_err, percentile)
+            del label_err
 
             # False positive background predictions.
-            bg_falsepos = np.sort(100 * bg_falsepos / true_bg_tot)
-            bg_falsepos = bg_falsepos[int(len(bg_falsepos) * (percentile / 100))]
-            d['bg_falsepos'][epoch] = bg_falsepos
+            bg_falsepos = 100 * falsepos_bg / num_bg
+            d['bg_falsepos'][epoch] = computePercentile(bg_falsepos, percentile)
             del bg_falsepos
 
             # False positive foreground predictions.
-            fg_falsepos = np.sort(100 * fg_falsepos / true_fg_tot)
-            fg_falsepos = fg_falsepos[int(len(fg_falsepos) * (percentile / 100))]
-            d['fg_falsepos'][epoch] = fg_falsepos
+            fg_falsepos = 100 * falsepos_fg / num_fg
+            d['fg_falsepos'][epoch] = computePercentile(fg_falsepos, percentile)
             del fg_falsepos
 
             # BBox error.
-            bb_err = np.hstack([_.bbox_err for _ in acc])
+            bb_err = np.hstack([_.bbox for _ in err])
             num_bb = bb_err.shape[1]
             if num_bb > 0:
-                tmp = np.sort(bb_err, axis=1)
-                tmp = tmp[:, int(num_bb * (percentile / 100))]
-                d['bb_err_x0'][epoch] = tmp[0]
-                d['bb_err_y0'][epoch] = tmp[1]
-                d['bb_err_x1'][epoch] = tmp[2]
-                d['bb_err_y1'][epoch] = tmp[3]
+                d['bb_err_x0'][epoch] = computePercentile(bb_err[0], percentile)
+                d['bb_err_y0'][epoch] = computePercentile(bb_err[1], percentile)
+                d['bb_err_x1'][epoch] = computePercentile(bb_err[2], percentile)
+                d['bb_err_y1'][epoch] = computePercentile(bb_err[3], percentile)
 
-                tmp = np.sort(tmp.flatten())
-                d['bb_err_all'][epoch] = tmp[int(len(tmp) * (percentile / 100))]
-                del tmp
+                bb_all = bb_err.flatten()
+                d['bb_err_all'][epoch] = computePercentile(bb_all, percentile)
             del bb_err, num_bb
     return data
 
@@ -91,7 +112,7 @@ def plotTrainingProgress(log):
     # indirectly from the number of recorded costs since there is exactly one
     # cost per sample.
     num_epochs = log['conf'].num_epochs
-    assert len(log['acc']) % num_epochs == 0
+    assert len(log['err']) % num_epochs == 0
     samples_per_epoch = len(log['cost']) // num_epochs
 
     # Compute percentiles of cost based on values in each epoch.
@@ -105,7 +126,7 @@ def plotTrainingProgress(log):
     # Find a decent cost range for the plots and round it to closest decade.
     min_cost = np.amin(cost_50p)
     max_cost = np.amax(cost_90p)
-    min_cost = 10 ** np.floor(np.log10(max(1, min_cost)))
+    min_cost = 10 ** np.floor(np.log10(max(1, min_cost)) - 1)
     max_cost = 10 ** np.ceil(np.log10(max_cost))
 
     vec_x = np.arange(num_epochs)
@@ -130,25 +151,24 @@ def plotTrainingProgress(log):
     for idx, layer_dim in enumerate(log['conf'].rpcn_out_dims):
         data = compileStatistics(log['rpcn'][layer_dim], num_epochs, samples_per_epoch)
 
-        # Cost of RPCN Layer.
+        # Cost of Fg/Bg decision.
         plt.subplot(num_rows, num_cols, num_cols * idx + 1)
-        plt.semilogy(vec_x, data['90p']['cost'], '-b', label='90%')
-        plt.semilogy(vec_x, data['50p']['cost'], '-r', label='Median')
-        pfill(vec_x, 1, data['90p']['cost'], facecolor='b', alpha=0.2)
-        pfill(vec_x, 1, data['50p']['cost'], facecolor='r', alpha=0.2)
+        plt.semilogy(vec_x, data['100p']['cost_isFg'], '-r', label='BG/FG')
+        plt.semilogy(vec_x, data['100p']['cost_cls'], '-g', label='Class')
+        plt.semilogy(vec_x, data['100p']['cost_bbox'], '-b', label='BBox')
 
         plt.xlim(min(vec_x), max(vec_x))
         plt.ylim(min_cost, max_cost)
         plt.grid()
         plt.legend(loc='best')
-        plt.title(f'Cost (Feature Size: {layer_dim[0]}x{layer_dim[1]})')
+        plt.title(f'Costs (Feature Size: {layer_dim[0]}x{layer_dim[1]})')
 
         # Classification error rate.
         plt.subplot(num_rows, num_cols, num_cols * idx + 2)
-        plt.plot(vec_x, data['90p']['fg_err'], '-b', label='90%')
-        plt.plot(vec_x, data['50p']['fg_err'], '-r', label='Median')
-        pfill(vec_x, 1, data['90p']['fg_err'], facecolor='b', alpha=0.2)
-        pfill(vec_x, 1, data['50p']['fg_err'], facecolor='r', alpha=0.2)
+        plt.plot(vec_x, data['90p']['bgfg_err'], '-b', label='90%')
+        plt.plot(vec_x, data['50p']['bgfg_err'], '-r', label='Median')
+        pfill(vec_x, 1, data['90p']['bgfg_err'], facecolor='b', alpha=0.2)
+        pfill(vec_x, 1, data['50p']['bgfg_err'], facecolor='r', alpha=0.2)
 
         plt.grid()
         plt.xlim(min(vec_x), max(vec_x))
