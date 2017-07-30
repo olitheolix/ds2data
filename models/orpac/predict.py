2ab14a470cccc4efaf60d26f5a28baa117c6057e
Oliver Nagy <olitheolix@gmail.com>
2017-07-30 12:22:19 +1000

    Removed code duplication.

--- models/orpac/predict.py
+++ models/orpac/predict.py
@@ -12,7 +12,6 @@ import numpy as np
 import tensorflow as tf
 import PIL.Image as Image
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 
 
 def parseCmdline():
@@ -37,59 +36,6 @@ def parseCmdline():
     return param
 
 
-def analyseImage(sess, x_in, int2name, rpcn_dims, fname):
-    """Predict BBoxes and draw them over the image.
-
-    Predicts BBoxes with every RPCN size specified in `rpcn_dims` and puts the
-    result into a new sub-plot.
-
-    Input:
-        sess: Tenseorflow sessions
-        x_in: Tensorflow Placeholder
-        int2name: Dict[int: str]
-            Mapping from numerical label to human readable one.
-        rpcn_dims: List[Tuple(ft_height, ft_width)]
-            Invoke only the RPCNs with those feature maps sizes. Each feature
-            map size must also be a key in `ys`.
-        fname: str
-            File name of image file.
-
-    Returns:
-        Matplotlib figure handle.
-    """
-    # Load test image and predict BBoxes for it.
-    img_hwc = np.array(Image.open(fname), np.float32) / 255
-    img_chw = np.transpose(img_hwc, [2, 0, 1])
-    pred = validate.predictBBoxes(sess, x_in, img_chw, rpcn_dims, None, int2name)
-    _, bb_rects, bb_labels, _ = pred
-
-    # Create the figure window and specify the Matplotlib rectangle parameters.
-    fig = plt.figure(figsize=(20, 11))
-    rect_opts = dict(linewidth=1, facecolor='none', edgecolor='g')
-
-    # Draw BBoxes over image. Create one figure for each feature map size.
-    num_cols = len(rpcn_dims)
-    for idx, ft_dim in enumerate(rpcn_dims):
-        # Display the original input image.
-        ax = plt.subplot(1, num_cols, idx + 1)
-        ax.set_axis_off()
-        ax.imshow(img_hwc)
-        plt.title(f'RPCN Size: {ft_dim[0]}x{ft_dim[1]}')
-
-        # Add the BBoxes.
-        for label, (x0, y0, x1, y1) in zip(bb_labels[ft_dim], bb_rects[ft_dim]):
-            w = x1 - x0 + 1
-            h = y1 - y0 + 1
-            ax.add_patch(patches.Rectangle((x0, y0), w, h, **rect_opts))
-            ax.text(
-                x0 + w / 2, y0, f' {int2name[label]} ',
-                bbox={'facecolor': 'black', 'pad': 0},
-                fontdict=dict(color='white', size=12, weight='normal'),
-                horizontalalignment='center', verticalalignment='center'
-            )
-    return fig
-
-
 def main():
     param = parseCmdline()
 
@@ -103,7 +49,7 @@ def main():
     # Simulation parameters.
     meta = pickle.load(open(fn_meta, 'rb'))
     conf, int2name = meta['conf'], meta['int2name']
-    rpcn_out_dims = conf.rpcn_out_dims
+    ft_dims = conf.rpcn_out_dims
     num_cls = len(int2name)
     im_dim = (3, conf.height, conf.width)
 
@@ -133,7 +79,21 @@ def main():
     print(f'\n-----Predicting BBoxes and saving results to {param.dst} -----')
     fig_opts = dict(dpi=150, transparent=True, bbox_inches='tight', pad_inches=0)
     for i, fname in enumerate(tqdm.tqdm(fnames)):
-        fig = analyseImage(sess, x_in, int2name, rpcn_out_dims, fname)
+        # Load the image and convert it to a CHW NumPy array.
+        img = (np.array(Image.open(fname), np.float32) / 255).transpose([2, 0, 1])
+
+        # Predict the BBoxes and labels.
+        tmp = validate.predictBBoxes(sess, x_in, img, ft_dims, None, int2name)
+        preds, pred_rect, pred_cls, true_cls = tmp
+
+        # Sanity check: there must not be any NaNs in the output.
+        for _ in preds.values():
+            assert not np.any(np.isnan(_))
+
+        # Create figure with original images and predicted BBoxes.
+        fig = validate.showPredictedBBoxes(img, pred_rect, pred_cls, true_cls, int2name)
+
+        # Save the figure.
         fname_out = os.path.basename(fname)
         fname_out = os.path.join(param.dst, f'pred-{fname_out}')
         fig.savefig(fname_out, **fig_opts)
