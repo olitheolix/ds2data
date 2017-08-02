531bfd8192eef886d9ac97307ed96399c49fbda4
Oliver Nagy <olitheolix@gmail.com>
2017-08-02 21:26:12 +1000

    Removed support for multiple feature map sizes.

--- models/orpac/inspect_feature.py
+++ models/orpac/inspect_feature.py
@@ -34,7 +34,10 @@ def parseCmdline():
     return param
 
 
-def plotMasksAndFeatures(img_hwc, ys, metas, int2name):
+def plotMasksAndFeatures(img_hwc, y, meta, int2name, ft_dim):
+    assert y.ndim == 4 and y.shape[0] == 1
+    y = y[0]
+
     num_classes = len(int2name)
     assert img_hwc.ndim == 3 and img_hwc.shape[2] == 3
 
@@ -51,83 +54,79 @@ def plotMasksAndFeatures(img_hwc, ys, metas, int2name):
     )
 
     num_rows, num_cols = 3, 5
-    for ft_dim in sorted(ys):
-        y = ys[ft_dim]
-        assert y.ndim == 3
-        meta = metas[ft_dim]
 
-        # Unpack the true foreground class labels and make hard decision.
-        true_labels = getClassLabel(ys[ft_dim])
-        true_labels = np.argmax(true_labels, axis=0)
+    # Unpack the true foreground class labels and make hard decision.
+    true_labels = getClassLabel(y)
+    true_labels = np.argmax(true_labels, axis=0)
 
-        # New figure window and title.
-        fig = plt.figure()
-        fig.canvas.set_window_title(f'{ft_dim[0]}x{ft_dim[1]}: {meta.filename}')
+    # New figure window and title.
+    fig = plt.figure()
+    fig.canvas.set_window_title(f'{ft_dim[0]}x{ft_dim[1]}: {meta.filename}')
 
-        plt.subplot(num_rows, num_cols, 1)
-        plt.imshow(img)
-        plt.title('Input Image')
+    plt.subplot(num_rows, num_cols, 1)
+    plt.imshow(img)
+    plt.title('Input Image')
 
-        plt.subplot(num_rows, num_cols, 2)
-        plt.imshow(meta.mask_fg, cmap='gray', clim=[0, 1])
-        plt.title('Foreground')
+    plt.subplot(num_rows, num_cols, 2)
+    plt.imshow(meta.mask_fg, cmap='gray', clim=[0, 1])
+    plt.title('Foreground')
 
-        plt.subplot(num_rows, num_cols, 3)
-        plt.imshow(meta.mask_bbox, cmap='gray', clim=[0, 1])
-        plt.title('BBox Estimation Possible')
+    plt.subplot(num_rows, num_cols, 3)
+    plt.imshow(meta.mask_bbox, cmap='gray', clim=[0, 1])
+    plt.title('BBox Estimation Possible')
 
-        plt.subplot(num_rows, num_cols, 4)
-        plt.imshow(meta.mask_cls, cmap='gray', clim=[0, 1])
-        plt.title('Label Estimation Possible')
+    plt.subplot(num_rows, num_cols, 4)
+    plt.imshow(meta.mask_cls, cmap='gray', clim=[0, 1])
+    plt.title('Label Estimation Possible')
 
-        plt.subplot(num_rows, num_cols, 5)
-        plt.imshow(meta.mask_valid, cmap='gray', clim=[0, 1])
-        plt.title('Valid')
+    plt.subplot(num_rows, num_cols, 5)
+    plt.imshow(meta.mask_valid, cmap='gray', clim=[0, 1])
+    plt.title('Valid')
 
-        # BBoxes over original image.
-        ax = plt.subplot(num_rows, num_cols, 6)
-        plt.imshow(img)
+    # BBoxes over original image.
+    ax = plt.subplot(num_rows, num_cols, 6)
+    plt.imshow(img)
 
-        hard = np.argmax(getClassLabel(y), axis=0)
-        bb_rects, pick_yx = unpackBBoxes(im_dim, getBBoxRects(y), hard)
-        label = hard[pick_yx]
-        for label, (x0, y0, x1, y1) in zip(label, bb_rects):
-            w = x1 - x0
-            h = y1 - y0
-            ax.add_patch(patches.Rectangle((x0, y0), w, h, **rect_opts))
-            ax.text(x0 + w / 2, y0, f' {int2name[label]} ', **txt_opts)
+    hard = np.argmax(getClassLabel(y), axis=0)
+    bb_rects, pick_yx = unpackBBoxes(im_dim, getBBoxRects(y), hard)
+    label = hard[pick_yx]
+    for label, (x0, y0, x1, y1) in zip(label, bb_rects):
+        w = x1 - x0
+        h = y1 - y0
+        ax.add_patch(patches.Rectangle((x0, y0), w, h, **rect_opts))
+        ax.text(x0 + w / 2, y0, f' {int2name[label]} ', **txt_opts)
 
-        # True label map.
-        plt.subplot(num_rows, num_cols, 7)
-        plt.imshow(true_labels, clim=[0, num_classes])
-        plt.title(f'True Label Map')
+    # True label map.
+    plt.subplot(num_rows, num_cols, 7)
+    plt.imshow(true_labels, clim=[0, num_classes])
+    plt.title(f'True Label Map')
 
-        # Densly sample the masks.
-        m_bbox, m_fg, m_cls = sampleMasks(
-            meta.mask_valid, meta.mask_fg, meta.mask_bbox,
-            meta.mask_cls, meta.mask_objid_at_pix, 10)
+    # Densly sample the masks.
+    m_bbox, m_fg, m_cls = sampleMasks(
+        meta.mask_valid, meta.mask_fg, meta.mask_bbox,
+        meta.mask_cls, meta.mask_objid_at_pix, 10)
 
-        # Object ID inside rendering engine at each pixel.
-        plt.subplot(num_rows, num_cols, 8)
-        plt.imshow(meta.mask_objid_at_pix)
-        plt.title('Object ID at Pixel')
+    # Object ID inside rendering engine at each pixel.
+    plt.subplot(num_rows, num_cols, 8)
+    plt.imshow(meta.mask_objid_at_pix)
+    plt.title('Object ID at Pixel')
 
-        # Sampled locations to estimate BBox dimensions.
-        plt.subplot(num_rows, num_cols, 11)
-        plt.imshow(true_labels * m_bbox, clim=[0, num_classes])
-        plt.title('Sampled BBox Locations')
+    # Sampled locations to estimate BBox dimensions.
+    plt.subplot(num_rows, num_cols, 11)
+    plt.imshow(true_labels * m_bbox, clim=[0, num_classes])
+    plt.title('Sampled BBox Locations')
 
-        # Sampled locations to estimate Class label.
-        plt.subplot(num_rows, num_cols, 12)
-        plt.imshow(true_labels * m_cls, clim=[0, num_classes])
-        plt.title('Sampled Class Locations')
+    # Sampled locations to estimate Class label.
+    plt.subplot(num_rows, num_cols, 12)
+    plt.imshow(true_labels * m_cls, clim=[0, num_classes])
+    plt.title('Sampled Class Locations')
 
-        # Sampled locations to estimate foreground/background.
-        plt.subplot(num_rows, num_cols, 13)
-        plt.imshow(m_fg, cmap='gray', clim=[0, 1])
-        plt.title('Sampled Fg/Bg Locations')
+    # Sampled locations to estimate foreground/background.
+    plt.subplot(num_rows, num_cols, 13)
+    plt.imshow(m_fg, cmap='gray', clim=[0, 1])
+    plt.title('Sampled Fg/Bg Locations')
 
-        plt.suptitle(f'Feature Map Size: {ft_dim[0]}x{ft_dim[1]}')
+    plt.suptitle(f'Feature Map Size: {ft_dim[0]}x{ft_dim[1]}')
 
 
 def main(data_path=None):
@@ -136,8 +135,8 @@ def main(data_path=None):
     # Dummy Net configuration. We only fill in the values for the Loader.
     conf = config.NetConf(
         seed=0, dtype='float32', path=data_path, train_rat=0.8,
-        num_pools_shared=None, rpcn_out_dims=[(64, 64), (32, 32)],
-        rpcn_filter_size=None, num_epochs=None, num_samples=None
+        layers=None, ft_dim=(64, 64),
+        filter_size=None, epochs=None, samples=None
     )
 
     # Load the data set and request a sample.
@@ -152,7 +151,7 @@ def main(data_path=None):
     img = np.transpose(x, [1, 2, 0])
 
     meta = ds.getMeta([uuid])[uuid]
-    plotMasksAndFeatures(img, y, meta, ds.int2name())
+    plotMasksAndFeatures(img, y, meta, ds.int2name(), conf.ft_dim)
 
     plt.show()
 
