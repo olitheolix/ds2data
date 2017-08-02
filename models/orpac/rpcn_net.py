56cc705ad1cfa497e0f84085b1abd6f919006522
Oliver Nagy <olitheolix@gmail.com>
2017-08-02 20:54:50 +1000

    Removed legacy model.

--- models/orpac/rpcn_net.py
+++ models/orpac/rpcn_net.py
@@ -8,7 +8,6 @@ the size of the input feature map. The idea is that smaller feature map
 correspond to a larger receptive field (the filter sizes are identical in all
 ORPAC layers)
 """
-import pickle
 import numpy as np
 import tensorflow as tf
 
@@ -16,29 +15,6 @@ import tensorflow as tf
 _SCALE_BBOX = 10
 _SCALE_ISFG = 3000
 _SCALE_CLS = 1000
-
-
-def model(x_in, bwt1, bwt2):
-    # Convenience
-    conv_opts = dict(padding='SAME', data_format='NCHW')
-    with tf.variable_scope(f'orpac'):
-        # Convolution layer to downsample the feature map.
-        # Shape: [-1, 64, 64, 64] ---> [-1, 64, 32, 32]
-        # Kernel: 3x3
-        b1 = tf.Variable(bwt1[0], trainable=bwt1[2], name='b1')
-        W1 = tf.Variable(bwt1[1], trainable=bwt1[2], name='W1')
-        net_out = tf.nn.conv2d(x_in, W1, [1, 1, 2, 2], **conv_opts)
-        net_out = tf.nn.relu(net_out + b1)
-
-        # Convolution layer to learn the BBoxes and class labels.
-        # Shape: [-1, 64, 32, 32] ---> [-1, 4 + 2 + num_classes, 64, 64]
-        # Kernel: 5x5
-        b2 = tf.Variable(bwt2[0], trainable=bwt2[2], name='b2')
-        W2 = tf.Variable(bwt2[1], trainable=bwt2[2], name='W2')
-
-        orpac_out = tf.nn.conv2d(net_out, W2, [1, 1, 1, 1], **conv_opts)
-        orpac_out = tf.add(orpac_out, b2, name='out')
-    return net_out, orpac_out
 
 
 def _crossEnt(logits, labels, name=None):
@@ -101,73 +77,6 @@ def cost(y_pred):
 
         # Compute final scalar cost.
         return tf.add_n([cost_bbox, cost_isFg, cost_cls], name='total')
-
-
-def save(fname, sess):
-    """ Save the pickled network state to `fname`.
-
-    Args:
-        fname: str
-           Name of pickle file.
-        sess: Tensorflow Session
-    """
-    # Query the state of the shared network (weights and biases).
-    g = tf.get_default_graph().get_tensor_by_name
-    state = {}
-    W1, b1 = sess.run([g(f'orpac/W1:0'), g(f'orpac/b1:0')])
-    W2, b2 = sess.run([g(f'orpac/W2:0'), g(f'orpac/b2:0')])
-    state = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
-
-    # Save the state.
-    pickle.dump(state, open(fname, 'wb'))
-
-
-def load(fname):
-    return pickle.load(open(fname, 'rb'))
-
-
-def setup(fname, x_in, num_classes, filter_size, trainable):
-    assert x_in.dtype in [tf.float16, tf.float32]
-    dtype = np.float16 if x_in.dtype == tf.float16 else np.float32
-    num_features_out = 64
-
-    print(f'ORPAC:')
-    print(f'  Restored from <{fname}>')
-
-    # Create non-maximum-suppression nodes. This is irrelevant for training but
-    # the predictor will need it and it is better to create the necessary
-    # variables and operations only once.
-    with tf.variable_scope('non-max-suppression'):
-        r_in = tf.placeholder(tf.float32, [None, 4], name='bb_rects')
-        s_in = tf.placeholder(tf.float32, [None], name='scores')
-        tf.image.non_max_suppression(r_in, s_in, 30, 0.2, name='op')
-
-    num_features_in = x_in.shape.as_list()[1]
-
-    W1_dim = (3, 3, num_features_in, num_features_out)
-    b1_dim = (num_features_out, 1, 1)
-    W2_dim = (filter_size, filter_size, num_features_out, 4 + 2 + num_classes)
-    b2_dim = (4 + 2 + num_classes, 1, 1)
-
-    if fname is None:
-        b1 = 0.5 + np.zeros(b1_dim).astype(dtype)
-        W1 = np.random.normal(0.0, 0.1, W1_dim).astype(dtype)
-        b2 = 0.5 + np.zeros(b2_dim).astype(dtype)
-        W2 = np.random.normal(0.0, 0.1, W2_dim).astype(dtype)
-    else:
-        net = load(fname)
-        b1, W1 = net['b1'], net['W1']
-        b2, W2 = net['b2'], net['W2']
-
-    assert b1.dtype == W1.dtype == dtype
-    assert b1.shape == b1_dim and W1.shape == W1_dim
-    assert b2.dtype == W2.dtype == dtype
-    assert b2.shape == b2_dim and W2.shape == W2_dim
-
-    bwt1 = (b1, W1, trainable)
-    bwt2 = (b2, W2, trainable)
-    net_out, orpac_out = model(x_in, bwt1, bwt2)
-    return orpac_out
 
 
 def unpackBiasAndWeight(bw_init, b_dim, W_dim, layer, dtype):
