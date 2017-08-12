2ec18069c224d1ab904b455eb115731e078cdc5c
Oliver Nagy <olitheolix@gmail.com>
2017-08-12 11:43:35 +1000

    Added 'containers' module to clean up the size/dimension mess.

---
+++ models/orpac/containers.py
@@ -0,0 +1,52 @@
+class Shape:
+    """Container to store 3D Image/array/feature/tensor sizes.
+
+    This is a convenience class because size specifications are often required
+    yet their format is ambigous. Sometimes, images are specified as CHW
+    (Tensorflow), sometimes as HWC (NumPy, Matplotlib). Sometimes, only the
+    width and height are needed which Tensorflow needs as (height, width) yet
+    eg. PIL returns as (width, height).
+
+    This container class accepts the three size parameters and can return them
+    in all possible formats.
+
+    Inputs:
+        chan: int
+            Number of channels. Must be non-negative or None.
+        height: int
+            Must be non-negative (can *not* be None).
+        width: int
+            Must be non-negative (can *not* be None).
+    """
+    def __init__(self, chan, height, width):
+        # Sanity checks.
+        assert chan is None or isinstance(chan, int) and chan >= 0
+        assert isinstance(width, int) and width >= 0
+        assert isinstance(height, int) and height >= 0
+
+        # Store the parameters.
+        self.chan = chan
+        self.height = height
+        self.width = width
+
+    def __eq__(self, ref):
+        try:
+            assert isinstance(ref, Shape)
+            assert ref.chan == self.chan
+            assert ref.height == self.height
+            assert ref.width == self.width
+            return True
+        except AssertionError:
+            return False
+
+    def chw(self):
+        return (self.chan, self.height, self.width)
+
+    def hwc(self):
+        return (self.height, self.width, self.chan)
+
+    def hw(self):
+        return (self.height, self.width)
+
+    def wh(self):
+        return (self.width, self.height)
---
+++ models/orpac/test_containers.py
@@ -0,0 +1,69 @@
+import pytest
+import containers
+
+
+class TestSize:
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def setup_method(self, method):
+        pass
+
+    def teardown_method(self, method):
+        pass
+
+    def test_size_basic(self):
+        chan, height, width = 3, 10, 20
+        s = containers.Shape(chan=chan, height=height, width=width)
+
+        assert s.chan == chan
+        assert s.height == height
+        assert s.width == width
+
+        assert s.chw() == (chan, height, width)
+        assert s.hwc() == (height, width, chan)
+        assert s.hw() == (height, width)
+        assert s.wh() == (width, height)
+
+    def test_size_err(self):
+        """Class must accept None value for channel only"""
+        # Must succeed because all values are non-negative integers and None is
+        # admissible for 'chan'.
+        containers.Shape(chan=None, height=0, width=10)
+
+        # Must fail because None is acceptable *only* for 'chan'.
+        with pytest.raises(AssertionError):
+            containers.Shape(chan=3, height=None, width=10)
+        with pytest.raises(AssertionError):
+            containers.Shape(chan=3, height=10, width=None)
+
+        # Class must reject floating point values.
+        with pytest.raises(AssertionError):
+            containers.Shape(chan=3, height=10.5, width=10)
+
+        # Class must reject negative values.
+        with pytest.raises(AssertionError):
+            containers.Shape(chan=3, height=-10, width=10)
+
+    def test_comparison(self):
+        s1 = containers.Shape(chan=1, height=2, width=3)
+        s2 = containers.Shape(chan=1, height=2, width=3)
+        s3 = containers.Shape(chan=1, height=2, width=4)
+
+        # All containers must be identical to themselves.
+        assert s1 == s1 and s2 == s2 and s3 == s3
+
+        # First two containers are different objects but encode the same
+        # dimensions. Therefore, they must be equal.
+        assert s1 == s2
+
+        # The third container is different.
+        assert s1 != s3 and s2 != s3
+
+        # Comparison to an arbitrary object (eg string) must return False.
+        assert s1 != 'foo'
